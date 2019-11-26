80e34184f601be30613c653f85b49c36fe5ceb3e
readlnh <readlnh@163.com>
2019-11-26 18:14:27 +0800

    use serde to save the data as json

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ extern crate rsa;
 extern crate serde_json;
 
 use clap::{App, SubCommand};
-use rand::rngs::OsRng;
-use rsa::{hash::Hashes, PaddingScheme, PublicKey, RSAPrivateKey, RSAPublicKey, BigUint};
-use rsa_der::{public_key_from_der, public_key_to_der};
+use rand::{rngs::OsRng};
+use rsa::{hash::Hashes, PaddingScheme, PublicKey, RSAPrivateKey, RSAPublicKey};
 use std::{io, io::prelude::*};
 
 fn generate_key() {
@@ -20,10 +19,33 @@ fn generate_key() {
     println!("正在生成{}位密钥...", bits);
     let key = RSAPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
 
-    let serialized = serde_json::to_string(&key).unwrap();
+    let serialized_pri = serde_json::to_string(&key).unwrap();
 
+    let pub_key: RSAPublicKey = key.clone().into();
+    let serialized_pub = serde_json::to_string(&pub_key).unwrap();
+
+    let mut flag = false;
     loop {
-        println!("将私钥保存到:");
+        if !flag {
+            println!("将私钥保存到:");
+            cin.clear();
+            io::stdin().read_line(&mut cin).expect("请输入一个有效地址");
+            let mut file: std::fs::File;
+            match std::fs::File::create(&cin.trim()) {
+                Err(error) => {
+                    println!("{} 不是一个有效地址", cin);
+                    println!("error: {} 请输入一个有效地址", error);
+                    continue;
+                }
+                Ok(f) => {
+                    flag = true;
+                    file = f;
+                    file.write_all(serialized_pri.as_bytes()).unwrap();
+                }
+            };
+        }
+
+        println!("将公钥保存到:");
         cin.clear();
         io::stdin().read_line(&mut cin).expect("请输入一个有效地址");
         let mut file: std::fs::File;
@@ -34,16 +56,11 @@ fn generate_key() {
             }
             Ok(f) => {
                 file = f;
-                file.write_all(serialized.as_bytes()).unwrap();
+                file.write_all(serialized_pub.as_bytes()).unwrap();
                 break;
             }
         };
     }
-
-    let der_publickey = public_key_to_der(&key.n().to_bytes_be(), &key.e().to_bytes_be());
-    let publickeybase64 = base64::encode(&der_publickey);
-
-    println!("公钥为: {}", publickeybase64);
 }
 
 fn signature() -> String {
@@ -76,36 +93,39 @@ fn signature() -> String {
         io::stdin()
             .read_line(&mut cin)
             .expect("Failed to read bits");
-        
         let msg = &cin.trim().as_bytes();
 
         let sig = private_key
             .sign::<Hashes>(PaddingScheme::PKCS1v15, None, msg)
             .expect("error");
-        
         return base64::encode(&sig);
     }
 }
 
 fn verify() -> bool {
     loop {
-        println!("请输入公钥:");
+        println!("请输入公钥位置:");
         let mut cin = String::new();
         io::stdin()
             .read_line(&mut cin)
             .expect("Failed to read bits");
-        let (n, e) = match public_key_from_der(&base64::decode(&cin.trim()).unwrap()) {
+        let file = match std::fs::File::open(&cin.trim()) {
+            Ok(f) => f,
             Err(error) => {
-                println!("这不是一个合法的公钥:");
-                println!("error {}", error);
+                println!("{} 不是一个有效地址", cin);
+                println!("error: {} 请输入一个有效地址", error);
                 continue;
-            }, 
-            Ok(key) => {
-                key
-            },
+            }
         };
 
-        let public_key = RSAPublicKey::new(BigUint::from_bytes_be(&n), BigUint::from_bytes_be(&e)).unwrap();
+        let public_key: RSAPublicKey = match serde_json::from_reader(&file) {
+            Err(error) => {
+                println!("{} 不是一个有效的公钥", cin);
+                println!("error: {}", error);
+                continue;
+            }
+            Ok(key) => key,
+        };
 
         println!("请输入签名信息:");
         cin.clear();
@@ -114,7 +134,6 @@ fn verify() -> bool {
             .expect("Failed to read bits");
         let msg_sig = base64::decode(&cin.trim()).unwrap();
 
-        
         println!("请输入原信息:");
         cin.clear();
         io::stdin()
@@ -122,14 +141,91 @@ fn verify() -> bool {
             .expect("Failed to read bits");
         let msg = cin.trim().as_bytes();
 
-    
         match public_key.verify::<Hashes>(PaddingScheme::PKCS1v15, None, &msg, &msg_sig) {
-            Ok(()) => { return true; }
-            Err(err) => { 
+            Ok(()) => {
+                return true;
+            }
+            Err(err) => {
                 println!("error: {}", err);
-                return false; 
-            }  
+                return false;
+            }
         }
+    }
+}
+
+fn encrypt() -> String {
+    loop {
+        println!("请输入公钥位置:");
+        let mut cin = String::new();
+        io::stdin()
+            .read_line(&mut cin)
+            .expect("Failed to read bits");
+        let file = match std::fs::File::open(&cin.trim()) {
+            Ok(f) => f,
+            Err(error) => {
+                println!("{} 不是一个有效地址", cin);
+                println!("error: {} 请输入一个有效地址", error);
+                continue;
+            }
+        };
+
+        let pub_key: RSAPublicKey = match serde_json::from_reader(&file) {
+            Err(error) => {
+                println!("{} 不是一个有效的公钥", cin);
+                println!("error: {}", error);
+                continue;
+            }
+            Ok(key) => key,
+        };
+
+        cin.clear();
+        println!("请输入需要加密的信息:");
+        io::stdin()
+            .read_line(&mut cin)
+            .expect("Failed to read bits");
+        let msg = &cin.trim().as_bytes();
+
+        let mut rng = OsRng::new().expect("no secure randomness available");
+        let sig = pub_key.encrypt(&mut rng, PaddingScheme::PKCS1v15, &msg).expect("failed to encrypt");
+        return base64::encode(&sig);
+    }
+}
+
+fn decrypt() -> String {
+    loop {
+        println!("请输入私钥位置:");
+        let mut cin = String::new();
+        io::stdin()
+            .read_line(&mut cin)
+            .expect("Failed to read bits");
+        let file = match std::fs::File::open(&cin.trim()) {
+            Ok(f) => f,
+            Err(error) => {
+                println!("{} 不是一个有效地址", cin);
+                println!("error: {} 请输入一个有效地址", error);
+                continue;
+            }
+        };
+
+        let private_key: RSAPrivateKey = match serde_json::from_reader(&file) {
+            Err(error) => {
+                println!("{} 不是一个有效的私钥", cin);
+                println!("error: {}", error);
+                continue;
+            }
+            Ok(key) => key,
+        };
+
+        println!("请输入密文:");
+        cin.clear();
+        io::stdin()
+            .read_line(&mut cin)
+            .expect("Failed to read bits");
+        let msg_enc = base64::decode(&cin.trim()).unwrap();
+
+
+        let dec_data = private_key.decrypt(PaddingScheme::PKCS1v15, &msg_enc).expect("Failed to decrypt");
+        return String::from_utf8(dec_data).unwrap();
     }
 }
 
@@ -141,6 +237,8 @@ fn main() {
         .subcommand(SubCommand::with_name("key").about("generate key pairs"))
         .subcommand(SubCommand::with_name("signature").about("sign with the private key"))
         .subcommand(SubCommand::with_name("verify").about("verify with the public key"))
+        .subcommand(SubCommand::with_name("encrypt").about("encrypt with the public key"))
+        .subcommand(SubCommand::with_name("decrypt").about("decrypt with the private key"))
         .get_matches();
 
     if let Some(_matches) = matches.subcommand_matches("key") {
@@ -153,31 +251,15 @@ fn main() {
     if let Some(_matches) = matches.subcommand_matches("verify") {
         if verify() {
             println!("验证通过");
-        }
-        else {
+        } else {
             println!("验证失败");
         }
     }
+    if let Some(_matches) = matches.subcommand_matches("encrypt") {
+        println!("密文为: {}", encrypt());
+    }
+    if let Some(_matches) = matches.subcommand_matches("decrypt") {
+        println!("明文为 {}", decrypt());
+    }
 
-    // Encrypt
-    /*let data = b"hello world";
-    let enc_data = key.encrypt(&mut rng, PaddingScheme::PKCS1v15, &data[..]).expect("failed to encrypt");
-    //println!("{:?}", &enc_data);
-    //assert_ne!(&data[..], &enc_data[..]);
-
-    // Decrypt
-    let dec_data = key.decrypt(PaddingScheme::PKCS1v15, &enc_data).expect("failed to decrypt");
-    println!("{:?}", str::from_utf8(&dec_data));
-
-    let msg = b"Thu Dec 19 18:06:16 EST 2013\n";
-    let xx = base64::encode(&msg);
-    println!("{}", xx);
-
-
-
-    //println!("{:?}", str::from_utf8(&expected_sig));
-
-    //publickey.verify().expect("wwwwwww");
-
-    //assert_eq!(&data[..], &dec_data[..]);*/
 }
